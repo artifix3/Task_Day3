2fab57692851671dc69a17712af5ab088ba8696f
yusif.pi <yusif.pi@CANR2-3>
2022-08-26 07:53:35 +0400

    Add project files.

---
+++ Task_Day3/Model/Insturuments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Day3.Model
+{
+   public abstract class Insturuments
+    {
+        public string InstrumentName { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string Body_Material { get; set; }
+        public string Color { get; set; }
+        public abstract string Voice();
+
+    }
+    public class Piano : Insturuments
+    {
+        public string Type { get; set; }
+        public string PedalType { get; set; }
+        public bool ToneCollector { get; set; }
+        public string  FrameType { get; set; }
+        public override string Voice()
+        {
+            return "Diririn";
+        }
+    }
+    public class Vibraphones : Insturuments
+    {
+      
+        public string Resonators { get; set; }
+        public string Range { get; set; }
+        public string PedalType { get; set; }
+        public override string Voice()
+        {
+            return "lololol";
+        }
+    }
+}
---
+++ Task_Day3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Task_Day3.Model;
+
+namespace Task_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insturuments[] insturumentsArrays =
+            {
+                new Piano
+                {
+                     InstrumentName = "C7X",
+                Brand = "Yamaha",
+                Price = 60.4,
+                Height = 40.5,
+                Weight = 50,
+                Body_Material = "Wood",
+                Color = "Black",
+                Type = "Grand Piano",
+                PedalType = "Shift/Damper",
+                ToneCollector = true,
+                FrameType = "V-pro Vacuum"
+                },
+                new Piano
+                {
+                    InstrumentName = "C6X",
+                 Brand = "Yamaha",
+                 Price = 100.4,
+                 Height = 42.5,
+                 Weight = 53,
+                 Body_Material = "Wood",
+                 Color = "Blue",
+                 Type = "Grand Piano",
+                 PedalType = "Shift/Sostenuto",
+                 ToneCollector = true,
+                 FrameType = "Shield Mold"
+                },
+                new Vibraphones
+                {
+                    InstrumentName = "YV-4110(M)",
+                Brand = "Yamaha",
+                Price = 300.4,
+                Height = 20.5,
+                Weight = 53,
+                Body_Material = "Wood/Iron",
+                Color = "Yellow",
+                Resonators= "Gold finish",
+                Range= "4 octaves; C28-C76",
+                PedalType= "Swivel"
+                },
+                new Vibraphones
+                {
+                InstrumentName = "YV-3910(M)",
+                Brand = "Yamaha",
+                Price = 290.4,
+                Height = 21.5,
+                Weight = 53,
+                Body_Material = "Wood/Iron",
+                Color = "Yellow",
+                Resonators = "Gold finish",
+                Range = "3.5 octaves; C28-F69",
+                PedalType = "oversized"
+                },
+
+            };
+
+            foreach (var item in insturumentsArrays)
+            {
+                foreach (var props in item.GetType().GetProperties())
+                {
+                    string val = props.GetValue(item).ToString();
+                    Console.WriteLine(val);
+                }
+            }
+
+            
+       
+            
+            
+        }
+    }
+}
